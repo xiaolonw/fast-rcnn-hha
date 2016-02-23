302838cb1307515a82efe8b9b1a0e8d5389c76d4
xiaolonw <xiaolonw@cs.cmu.edu>
2016-02-23 18:16:09 -0500

    255

--- lib/fast_rcnn/test.py
+++ lib/fast_rcnn/test.py
@@ -34,7 +34,7 @@ def _get_image_blob(im):
     """
     im_orig = im.astype(np.float32, copy=True)
     im_orig -= 127.5
-    im_orig = im_orig / 127.5
+    # im_orig = im_orig / 127.5
 
     im_shape = im_orig.shape
     im_size_min = np.min(im_shape[0:2])
